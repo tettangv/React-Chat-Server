42f78fafd0334cf3fc9f79cfcc55f7968c0c4357
tettangv <tettangv@gmail.com>
2019-10-08 23:19:37 +0700

    Update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const server = require("http").createServer();
 const io = require("socket.io")(server);
 
 io.on("connection", function(socket) {
-  spcket.on("emit", function() {
+  socket.on("emit", function() {
     console.log("Receive Data");
   });
 });

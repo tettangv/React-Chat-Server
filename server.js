2b471c836860c46382deb940348bec2f9baf29b6
tettangv <tettangv@gmail.com>
2019-10-08 22:50:01 +0700

    init

---
+++ server.js
@@ -0,0 +1,12 @@
+const server = require("http").createServer();
+const io = require("socket.io")(server);
+
+io.on("connection", function(socket) {
+  spcket.on("emit", function() {
+    console.log("Receive Data");
+  });
+});
+server.listen(8080, function(err) {
+  if (err) throw err;
+  console.log("server is listening to port 8080");
+});

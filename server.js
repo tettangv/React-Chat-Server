15caccee596a8d82d64f13e22d1b1a8c3c9053ab
tettangv <tettangv@gmail.com>
2019-10-11 21:56:42 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,10 +2,17 @@ const server = require("http").createServer();
 const io = require("socket.io")(server);
 
 io.on("connection", function(socket) {
-  socket.on("emit", function() {
-    console.log("Receive Data");
+  socket.on("emit", function(data) {
+    // console.log("Receive Data");
+    // console.log(data);
+    // console.log("_______________________");
+    // count++;
+    socket.broadcast.emit("count", { count: data.count });
+    console.log({ ...data });
+    socket.broadcast.emit("message", { ...data });
   });
 });
+
 server.listen(8080, function(err) {
   if (err) throw err;
   console.log("server is listening to port 8080");
